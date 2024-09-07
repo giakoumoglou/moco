06fed7403f25787a985818a3c3035648399136f2
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-09-07 17:57:13 +0300

    Update loader.py

--- moco/loader.py
+++ moco/loader.py
@@ -10,7 +10,6 @@ from PIL import ImageFilter
 
 class TwoCropsTransform:
     """Take two random crops of one image as the query and key."""
-
     def __init__(self, base_transform):
         self.base_transform = base_transform
 
@@ -22,7 +21,6 @@ class TwoCropsTransform:
 
 class GaussianBlur:
     """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
-
     def __init__(self, sigma=[0.1, 2.0]):
         self.sigma = sigma
 
