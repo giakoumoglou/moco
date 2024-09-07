58d960450723592f26c9c12dbe417ce2f2fdfac8
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-09-07 18:22:25 +0300

    Update main_moco.py

--- main_moco.py
+++ main_moco.py
@@ -205,7 +205,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     cudnn.benchmark = True
 
-    # Data loading code
+    # data loading code
     traindir = os.path.join(args.data, "train")
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     
